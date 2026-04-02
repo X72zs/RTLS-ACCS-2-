ef5acd8261abda4b74ac5cf792af3cfb48466f5f
abumisferFMqa <Thechevv@gmail.com>
2026-04-02 17:38:50 +0300

    d

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
 // =============================================
 // Admin Credentials
 // =============================================
-const ADMIN_USERNAME = "admin";
+const ADMIN_USERNAME = "admin@";
 const ADMIN_PASSWORD = "admin123";
 
 // =============================================
